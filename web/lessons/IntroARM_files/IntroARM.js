cfaf7bfbb7fa2ae4956cd5825b50a752911258d4
waldo-irc <axparsa@gmail.com>
2018-05-15 23:27:01 -0400

    Add files via upload

---
+++ web/lessons/IntroARM_files/IntroARM.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20140122
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('IntroARM_files/IntroARMMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');detectBrowser();fixAllIEPNGs('Media/transparent.gif');Widget.onload();performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
